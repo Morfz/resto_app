80d49bdd4c2f87ae34b6a826e99b464b94ffaa34
Morfz <100391684+Morfz@users.noreply.github.com>
2023-06-10 20:11:42 +0800

    Update index.blade.php

--- resources/views/menus/index.blade.php
+++ resources/views/menus/index.blade.php
@@ -25,7 +25,7 @@
         <div class="menu-box over-slider">
             @foreach ($makananUtama->menus as $menu)
                 <div class="menu-item">
-                    <img src="{{ asset('storage/' . $menu->image) }}">
+                    <img src="{{ Storage::url($menu->image) }}">
                     <div class="menu__info">
                         <div class="menu__info-top">
                             <h2>{{ $menu->name }}</h2>
