ee809409b04321eb83e76f9ed709043f54095d04
Morfz <100391684+Morfz@users.noreply.github.com>
2023-06-12 00:09:01 +0800

    Update index.blade.php

--- resources/views/reservations/index.blade.php
+++ resources/views/reservations/index.blade.php
@@ -28,19 +28,52 @@
                     </span>
                     <input class="input-field" type="datetime-local" id="date" name="date" min="{{ $min_date->format('Y-m-d H:i:s') }}" max="{{ $max_date->format('Y-m-d H:i:s') }}">
                     <span>
-                        <ion-icon name="person-outline"></ion-icon>
-                        <select id="table_id" name="table_id" class="input-field" required>
-                            @foreach($tables as $table)
-                                <option value="{{ $table->id }}">{{ $table->name }} ({{ $table->capacity }} Guests)</option>
-                            @endforeach
-                        </select>
-                        <ion-icon name="chevron-down-outline"></ion-icon>
+                        <button class="btn btn-secondary" id="popupButton" data-text="Know you will enjoy it">
+                            <span>Pilih Meja</span>
+                        </button>
+                        <div id="popupContainer" style="display: none;">
+                            <!-- Konten pop-up di sini -->
+                            <ion-icon name="person-outline"></ion-icon>
+                            <select id="table_id" name="table_id" class="input-field" required>
+                                @foreach($tables as $table)
+                                    <option value="{{ $table->id }}">{{ $table->name }} ({{ $table->capacity }} Guests)</option>
+                                @endforeach
+                            </select>
+                            <ion-icon name="chevron-down-outline"></ion-icon>
+                            <button id="closeButton">Tutup Pop-up</button>
+                        </div>  
                     </span>
                 </div>
             </div>
-            <button class="btn btn-secondary" type="submit" data-toggle="modal" data-target="#contohModal" data-text="Know you will enjoy it">
+            <button class="btn btn-secondary" type="submit" data-text="Know you will enjoy it">
                 <span>Book A Table</span>
             </button>
         </form>
     </section>
+    <style>
+        #popupContainer {
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            background: #fff;
+            padding: 20px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+            z-index: 9999;
+        }
+    </style>
+    <script>
+        var popupButton = document.getElementById('popupButton');
+        var popupContainer = document.getElementById('popupContainer');
+        var closeButton = document.getElementById('closeButton');
+    
+        popupButton.addEventListener('click', function() {
+            popupContainer.style.display = 'block';
+        });
+    
+        closeButton.addEventListener('click', function() {
+            popupContainer.style.display = 'none';
+        });
+    </script>        
+      
 </x-guest-layout>
