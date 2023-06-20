a4c65e04f9e377edf9505b1bbe88f082357fccc0
Morfz <100391684+Morfz@users.noreply.github.com>
2023-06-21 01:28:15 +0800

    Update WelcomeController.php

--- app/Http/Controllers/Frontend/WelcomeController.php
+++ app/Http/Controllers/Frontend/WelcomeController.php
@@ -11,17 +11,19 @@ class WelcomeController extends Controller
     public function index()
     {
         $specials = Category::where('name', 'Menu Spesial')->first();
+        $randomMenu = null; // Inisialisasi variabel $randomMenu dengan nilai null
+
         if ($specials) {
             $randomMenu = $specials->menus()->inRandomOrder()->first();
             // Rest of your code
         } else {
             // Handle the case when the "Menu Spesial" category is not found
         }
+
         $categories = Category::whereNotIn('name', ['Menu Spesial'])
             ->inRandomOrder()
             ->limit(3)
             ->get();
-
 
         return view('welcome', compact('specials', 'randomMenu', 'categories'));
     }
